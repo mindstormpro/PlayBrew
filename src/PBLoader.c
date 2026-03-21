82c0258068e492bf18d69cabe6e9a248183a85a8
mindstormpro <169100823+mindstormpro@users.noreply.github.com>
2026-03-21 14:27:42 -0700

    Update API table
    added some basic graphics APIs and their addresses, as well as commented syntax for each API function

--- src/PBLoader.c
+++ src/PBLoader.c
@@ -5,10 +5,17 @@
 
 #define NULL ((void *)0)
 
+//functions for 3.0.2 -- subject to change
+
 typedef struct {
-    void *(*sd_open)(const char *, int);
-    void (*sd_close)(void *);
-    void *(*sd_read)(const char *, int *, int);
+    void *(*sd_open)(const char *, int); //file path
+    void (*sd_close)(void *); //file handle
+    void *(*sd_read)(const char *, int *, int); //file handle, buffer, bytes
+
+    void (*gfx_clear)(int); //color
+    void (*gfx_drawLine)(int, int, int, int, int, int); //x1, y1, x2, y2, width, color
+    void (*gfx_setPixel)(int, int, int) //x, y, color 
+
     int (*printf)(const char *__format, ...);
 } ApiTable;
 
@@ -26,10 +33,16 @@ void myActualUpdateFunc(void) {
     api.sd_open  = (void *(*)(const char *, int))0x24067594;
     api.sd_close = (void (*)(void *))0x240676fc;
     api.sd_read  = (void *(*)(const char *, int *, int))0x24067918;
+    api.gfx_clear = (void (*)(int))0x24055d5c;
+    api.gfx_drawLine = (void (*)(int, int, int, int, int, int))0x24055be5;
+    api.gfx_setPixel = (void (*)(int, int, int))0x24055901;
     api.printf = (int (*)(const char *, ...))0x240baf50;
 
-    void *fh = api.sd_open("/randomFile.bin", 0x80 | kFileRead);
-    
+    void *fh = api.sd_open("/payload.bin", 0x80 | kFileRead);
+    if (fh == NULL) {
+        return;
+    }
+    api.printf("loaded /payload.bin!");
     int filesz;
     api.sd_read(fh, &filesz, sizeof(int));
     api.sd_read(fh, &entryPoint, sizeof(int));
