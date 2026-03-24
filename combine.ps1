9f4c3fb29a5c4ef8c6286e3f40ee14a90a71c454
mindstormpro <169100823+mindstormpro@users.noreply.github.com>
2026-03-24 09:53:37 -0700

    added padding to combine.ps1
    added padding to combine.ps1 and tweaked the .gitignore

--- combine.ps1
+++ combine.ps1
@@ -1,6 +1,15 @@
+$fwSize = Get-Item "pdfw-b"
+$size = $fwSize.length
+$padding = 4 - ($fw.Length % 4)
+
 $fw = [System.IO.File]::ReadAllBytes("pdfw-b")
-$patch = [System.IO.File]::ReadAllBytes("patcher.bin")
-$combined = New-Object byte[] ($fw.Length + $patch.Length)
-$fw.CopyTo($combined, 0)
-$patch.CopyTo($combined, $fw.Length)
+$patch = [System.IO.File]::ReadAllBytes("loader-b.bin")
+$combined = New-Object byte[] ($fw.Length + $patch.Length + $padding)
+
+if ($padding -gt 0) {
+    $bytes = [byte[]]::new($padding)
+    $bytes.CopyTo($combined, $fw.Length)
+}
+
+$patch.CopyTo($combined, $fw.Length + $padding)
 [System.IO.File]::WriteAllBytes("firmware_patched.bin", $combined)
