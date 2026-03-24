f920f3251258269890dda11e379f47cafc68efc2
scratchminer <scratchminer@gmail.com>
2026-03-24 15:47:34 -0400

    misc. fixes

--- include/playbrew.h
+++ include/playbrew.h
@@ -1,12 +1,12 @@
 #include <stddef.h>
 
 typedef struct {
-	void *(*sd_open)(const char *, int); //path, mode, returns file handle as a void ptr
-	void (*sd_close)(void *); // file handle
-	void *(*sd_read)(const char *, void *, size_t); //file handle, ptr to buffer, how much to read in bytes
-	int (*printf)(const char *format, ...);
-	void (*gfx_clear)(int); //color
-    void (*gfx_drawLine)(int, int, int, int, int, int); //x1, y1, x2, y2, width, color
-    void (*gfx_setPixel)(int, int, int) //x, y, color 
+	void *(*sd_open)(const char *path, int mode);
+	void (*sd_close)(void *fh);
+	int (*sd_read)(void *fh, void *buf, size_t len);
+	void (*printf)(const char *format, ...);
+	void (*gfx_clear)(int color);
+	void (*gfx_drawLine)(int x1, int x2, int y1, int y2, int width, int color);
+	void (*gfx_setPixel)(int x, int y, int color);
 } PlayBrewAPI;
 
