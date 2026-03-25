2746cc3e98d9506c48108864f33b2cd100ea04c5
mindstormpro <169100823+mindstormpro@users.noreply.github.com>
2026-03-25 09:57:30 -0700

    replace combine.ps1 with combine.py
    rewrote combine.ps1 in python to make it OS cross-compatible and to make it easier to understand and read.

---
+++ combine.py
@@ -0,0 +1,12 @@
+with open("pdfw-b", "rb") as f:
+     fw = f.read()
+
+with open("loader-b.bin", "rb") as f:
+    patch = f.read()
+
+padding = (4 - (len(fw) % 4)) % 4
+fw_padded = fw + b'\x00' * padding
+combined = fw_padded + patch
+
+with open("firmware_patched.bin", "wb") as f:
+    f.write(combined)
