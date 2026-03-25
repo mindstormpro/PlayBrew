ec2c611be5340405e07dc055f0407102ed21b4c7
mindstormpro <169100823+mindstormpro@users.noreply.github.com>
2026-03-25 10:01:04 -0700

    Update combine.py
    made it so the loader and the full patch are taken/put directly in the builds folder
    also removed a random space

--- combine.py
+++ combine.py
@@ -1,12 +1,12 @@
 with open("pdfw-b", "rb") as f:
-     fw = f.read()
+    fw = f.read()
 
-with open("loader-b.bin", "rb") as f:
+with open("build/loader-b.bin", "rb") as f:
     patch = f.read()
 
 padding = (4 - (len(fw) % 4)) % 4
 fw_padded = fw + b'\x00' * padding
 combined = fw_padded + patch
 
-with open("firmware_patched.bin", "wb") as f:
+with open("build/firmware_patched.bin", "wb") as f:
     f.write(combined)
