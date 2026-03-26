5a802a6206a0bffe18dae4c90e25b943287c9ce4
scratchminer <scratchminer@gmail.com>
2026-03-26 17:53:44 -0400

    clean up python code

--- combine.py
+++ combine.py
@@ -1,40 +1,40 @@
 from hashlib import md5
 
-print("Enter target system\nA for Rev. A, B for rev. B, or AB for both\n")
+print("Enter target system: A for Rev. A, B for Rev. B, or AB for both")
 target = input("|)> ")
 
-if target == "A" or target == "AB":
+if "A" in target.upper():
 	with open("pdfw-a", "rb") as f:
 		fw_header = bytearray(f.read(32))
 		fw = f.read()
-
+	
 	while bool(len(fw) & 3):
 		fw += b"\x00"
-
+	
 	with open("build/loader-a.bin", "rb") as f:
 		fw += f.read()
-
+	
 	fw_header[8:12] = len(fw).to_bytes(4, byteorder="little")
 	fw_header[24:] = md5(fw).digest()[:8]
-
+	
 	with open("build/pdfw-a-patched.bin", "wb") as f:
 		f.write(fw_header)
 		f.write(fw)
 
-if target == "B" or target == "AB":
+if "B" in target.upper():
 	with open("pdfw-b", "rb") as f:
 		fw_header = bytearray(f.read(32))
 		fw = f.read()
-
+	
 	while bool(len(fw) & 3):
 		fw += b"\x00"
-
+	
 	with open("build/loader-b.bin", "rb") as f:
 		fw += f.read()
-
+	
 	fw_header[8:12] = len(fw).to_bytes(4, byteorder="little")
 	fw_header[24:] = md5(fw).digest()[:8]
-
+	
 	with open("build/pdfw-b-patched.bin", "wb") as f:
 		f.write(fw_header)
 		f.write(fw)
