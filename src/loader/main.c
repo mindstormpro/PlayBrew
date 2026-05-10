24139dcad7ed9e26e6121a38d97950d8c0c6485a
Eric Lewis <ericlewis777@gmail.com>
2026-05-10 00:30:49 -0400

    Validate payload reads and entrypoint bounds
    Why: payload.bin can be missing, cut short, or changed. The loader used to trust the size and entry point from that file without checking them. A bad file could make the loader read into the wrong memory or jump outside the payload. This checks the reads and only runs the payload when the size and entry point are safe.

--- src/loader/main.c
+++ src/loader/main.c
@@ -1,16 +1,51 @@
 #include "3.0.2.h"
 
+#include <stdint.h>
 
 #ifdef TARGET_DVT1
-#define PLAYDATE_RAM_END ((void *)0x61000000)
+#define PLAYDATE_RAM_END ((uintptr_t)0x61000000u)
 #endif
 #ifdef TARGET_H7D1
-#define PLAYDATE_RAM_END ((void *)0x91000000)
+#define PLAYDATE_RAM_END ((uintptr_t)0x91000000u)
 #endif
 
+#define PLAYBREW_PAYLOAD_ALIGNMENT ((uintptr_t)4u)
+#define PLAYBREW_PAYLOAD_ALIGNMENT_MASK (PLAYBREW_PAYLOAD_ALIGNMENT - 1u)
+#define PLAYBREW_MAX_PAYLOAD_SIZE ((uintptr_t)(8u * 1024u * 1024u))
+#define PLAYBREW_SAFE_LOAD_REGION_START (PLAYDATE_RAM_END - PLAYBREW_MAX_PAYLOAD_SIZE)
+
+static int playbrew_read_exact(PlayBrewAPI *api, void *fh, void *buf, size_t len) {
+	return api->sd_read(fh, buf, len) == (int)len;
+}
+
+static int playbrew_payload_start(uint32_t payloadSize, uintptr_t *payloadStartAddress) {
+	if (payloadSize == 0u) {
+		return 0;
+	}
+
+	uintptr_t payloadSizeAddress = (uintptr_t)payloadSize;
+	if (payloadSizeAddress > UINTPTR_MAX - PLAYBREW_PAYLOAD_ALIGNMENT_MASK) {
+		return 0;
+	}
+
+	uintptr_t alignedSize = (payloadSizeAddress + PLAYBREW_PAYLOAD_ALIGNMENT_MASK) & ~PLAYBREW_PAYLOAD_ALIGNMENT_MASK;
+	if (alignedSize > PLAYBREW_MAX_PAYLOAD_SIZE) {
+		return 0;
+	}
+
+	uintptr_t startAddress = PLAYDATE_RAM_END - alignedSize;
+	if (startAddress < PLAYBREW_SAFE_LOAD_REGION_START) {
+		return 0;
+	}
+
+	*payloadStartAddress = startAddress;
+	return 1;
+}
+
 void playbrew_loader_load(void) {
-	void *payloadStart;
-	unsigned int entryPoint;
+	uint32_t payloadSize;
+	uint32_t entryOffset;
+	uintptr_t payloadStartAddress;
 	
 	PlayBrewAPI api;
 	
@@ -25,7 +60,9 @@ void playbrew_loader_load(void) {
 	api.printf("started PlayBrew! ");
 
 	void *log = api.sd_open("/PBLogs.txt", 0x88);
-	api.sd_close(log);
+	if (log != NULL) {
+		api.sd_close(log);
+	}
 
 	void *fh = api.sd_open("/payload.bin", 0x83);
 	
@@ -34,20 +71,33 @@ void playbrew_loader_load(void) {
 		return;
 	}
 	
-	unsigned int filesz;
-	api.sd_read(fh, &filesz, sizeof(unsigned int));
-	api.sd_read(fh, &entryPoint, sizeof(unsigned int));
-	
-	if (filesz % 4 != 0) {
-		payloadStart = PLAYDATE_RAM_END - (filesz + 4 - (filesz % 4));
-	}
-	else {
-		payloadStart = PLAYDATE_RAM_END - filesz;
+	if (!playbrew_read_exact(&api, fh, &payloadSize, sizeof(payloadSize)) ||
+		!playbrew_read_exact(&api, fh, &entryOffset, sizeof(entryOffset))) {
+		api.printf("could not read payload header! Returning! ");
+		api.sd_close(fh);
+		return;
 	}
 	
-	api.sd_read(fh, (void *)payloadStart, filesz);
+	if (!playbrew_payload_start(payloadSize, &payloadStartAddress)) {
+		api.printf("invalid payload size! Returning! ");
+		api.sd_close(fh);
+		return;
+	}
+
+	if (entryOffset >= payloadSize) {
+		api.printf("invalid payload entry point! Returning! ");
+		api.sd_close(fh);
+		return;
+	}
 	
-	void (*entry)(PlayBrewAPI *) = (void (*)(PlayBrewAPI *))((unsigned int)(payloadStart + entryPoint) | 1);
+	if (!playbrew_read_exact(&api, fh, (void *)payloadStartAddress, payloadSize)) {
+		api.printf("could not read payload body! Returning! ");
+		api.sd_close(fh);
+		return;
+	}
+	
+	uintptr_t entryAddress = payloadStartAddress + (uintptr_t)entryOffset;
+	void (*entry)(PlayBrewAPI *) = (void (*)(PlayBrewAPI *))(entryAddress | 1u);
 	api.sd_close(fh);
 	
 	api.printf("running the payload! ");
