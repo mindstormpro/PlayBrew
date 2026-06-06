76c34c332aad26501296e67d7d35f04790bc6a9f
mindstormpro <169100823+mindstormpro@users.noreply.github.com>
2026-06-06 11:41:50 -0700

    less incomplete uC_FS implementation scratchminer please save me from the depths of pointer hell

--- include/3.0.2.h
+++ include/3.0.2.h
@@ -6,7 +6,7 @@
 typedef struct {
 	void *(*ucfs_open)(const char *path, int mode);
 	void (*ucfs_close)(void *fh);
-	int (*sd_read)(void *fh, void *buf, size_t len);
+	int (*ucfs_read)(void *fh, void *buf, size_t len);
 	void (*printf)(const char *format, ...);
 	void (*gfx_clear)(int color);
 	void (*gfx_drawLine)(int x1, int x2, int y1, int y2, int width, int color);
@@ -15,9 +15,9 @@ typedef struct {
 
 
 
-#define ADDR_UCFS_OPEN ((void *(*)(const char *, int))0xplaceholder)
-#define ADDR_UFCS_CLOSE ((void (*)(void *))0x200177a4)
-#define ADDR_UCFS_READ ((size_t (*)(void *, size_t , size_t , void *))0x2001794)
+#define ADDR_UCFS_OPEN ((void *(**)(const char *, int))(0x2001778C + 0x14))
+#define ADDR_UCFS_CLOSE ((void (**)(void *))(0x2001778C + 0x18))
+#define ADDR_UCFS_READ ((int (*)(void *, void *, size_t))(0x2001778C + 0x1c))
 #define ADDR_PRINTF ((void (*)(const char *, ...))0x240baf51)
 #define ADDR_GFX_CLEAR ((void (*)(int))0x24055d5d)
 #define ADDR_GFX_DRAWLINE ((void (*)(int, int, int, int, int, int))0x24055be5)
--- src/loader/main.c
+++ src/loader/main.c
@@ -13,17 +13,17 @@ void playbrew_loader_load(void) {
 	unsigned int entryPoint;
 
 	PlayBrewAPI api;
-	void *temp = ADDR_UCFS_CLOSE;
 
-	api.ucfs_open = (void *(*)(const char *, int))ADDR_UCFS_OPEN;
-	api.ucfs_close = (void (*)(void *))*temp;
-	api.ucfs_read = (int (*)(void *, void *, size_t))ADDR_UCFS_READ;
+
+	api.ucfs_open = (void *(*)(const char *, int))*(ADDR_UCFS_OPEN);
+	api.ucfs_close = (void (*)(void *))*(ADDR_UCFS_CLOSE);
+	api.ucfs_read = (int (*)(void *, void *, size_t))*(ADDR_UCFS_READ);
 	api.printf = (void (*)(const char *, ...))ADDR_PRINTF;
 	api.gfx_clear = (void (*)(int))ADDR_GFX_CLEAR;
 	api.gfx_drawLine = (void (*)(int, int, int, int, int, int))ADDR_GFX_DRAWLINE;
 	api.gfx_setPixel = (void (*)(int, int, int))ADDR_GFX_SETPIXEL;
 	api.printf("  started PlayBrew!  ");
-	void *fh = api.fopen("/playbrew/payload.bin", "rb");
+	void *fh = api.ucfs_open("/playbrew/payload.bin", 0x83);
 
 	if (fh == NULL) {
 		api.printf("   could not open /playbrew/payload.bin! Returning!   ");
@@ -31,8 +31,8 @@ void playbrew_loader_load(void) {
 	}
 
 	unsigned int filesz;
-	api.fread(&filesz, sizeof(unsigned int), 1, fh);
-	api.fread(&entryPoint, sizeof(unsigned int), 1, fh);
+	api.ucfs_read(fh, &filesz, sizeof(unsigned int));
+	api.ucfs_read(fh, &entryPoint, sizeof(unsigned int));
 	if (filesz % 4 != 0) {
 		payloadStart = PLAYDATE_RAM_END - (filesz + 4 - (filesz % 4));
 	}
@@ -40,8 +40,8 @@ void playbrew_loader_load(void) {
 		payloadStart = PLAYDATE_RAM_END - filesz;
 	}
 	
-	api.fread((void *)payloadStart, filesz, 1, fh);
-	api.fclose(fh);
+	api.ucfs_read(fh, (void *)payloadStart, filesz);
+	api.ucfs_close(fh);
 	api.printf("  Finished all reading and closed the payload!  ");
 
 	api.printf("  running the payload!  ");
