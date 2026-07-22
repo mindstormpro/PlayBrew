cb8d85e890d03abb79ae30a59be161d927361315
mindstormpro <169100823+mindstormpro@users.noreply.github.com>
2026-07-21 18:14:01 -0700

    frick it: restarting from scratch with a better idea

--- include/3.0.2.h
+++ include/3.0.2.h
@@ -3,39 +3,4 @@
 
 #include <stddef.h>
 
-typedef struct {
-	void *(*ucfs_open)(char *path, int mode);
-	void (*ucfs_close)(void *fh);
-	int (*ucfs_read)(void *fh, void *buf, size_t len);
-
-	void (*printf)(const char *format, ...);
-	char *(*strcpy)(char *dest, const char *src);
-
-	void *(*malloc)(size_t size);
-
-	void (*gfx_clear)(int color);
-	void (*gfx_drawLine)(int x1, int x2, int y1, int y2, int width, int color);
-	void (*gfx_setPixel)(int x, int y, int color);
-} PlayBrewAPI;
-
-// ALL addresses must end in an ODD number to run the function in thumb mode and NOT ARM MODE
-// NOT DOING THAT MAKES THE DEVICE CRASH :(
-
-// also in hexadecimal it goes 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, a, b, c, d, e, f
-// that means that a, c, and e are technically even numbers while b, d, and f are not
-
-#define ADDR_UCFS_OPEN ((void *(*)(char *, int))0x2405078f)
-#define ADDR_UCFS_CLOSE ((void (*)(void *))0x240507c5)
-#define ADDR_UCFS_READ ((int (*)(void *, void *, size_t))0x240507ef)
-
-#define ADDR_PRINTF ((void (*)(const char *, ...))0x240baf51)
-#define ADDR_STRCPY ((char *(*)(char *, const char *))0x240bba9d)
-
-#define ADDR_MALLOC ((void *(*)(size_t))0x240b9de5) 
-
-#define ADDR_GFX_CLEAR ((void (*)(int))0x24055d5d)
-#define ADDR_GFX_DRAWLINE ((void (*)(int, int, int, int, int, int))0x24055be5)
-#define ADDR_GFX_SETPIXEL ((void (*)(int, int, int))0x24055901)
-
-
 #endif
--- src/loader/main.c
+++ src/loader/main.c
@@ -8,61 +8,7 @@
 #define PLAYDATE_RAM_END ((void *)0x91000000)
 #endif
 
-void playbrew_loader_load(void) {
-	void *payloadStart;
-	unsigned int entryPoint;
-
-	PlayBrewAPI api;
-
-
-	api.ucfs_open = (void *(*)(char *, int))ADDR_UCFS_OPEN;
-	api.ucfs_close = (void (*)(void *))ADDR_UCFS_CLOSE;
-	api.ucfs_read = (int (*)(void *, void *, size_t))ADDR_UCFS_READ;
-	api.printf = (void (*)(const char *, ...))ADDR_PRINTF;
-	api.strcpy = (char *(*)(char *, const char *))ADDR_STRCPY;
-	api.gfx_clear = (void (*)(int))ADDR_GFX_CLEAR;
-	api.gfx_drawLine = (void (*)(int, int, int, int, int, int))ADDR_GFX_DRAWLINE;
-	api.gfx_setPixel = (void (*)(int, int, int))ADDR_GFX_SETPIXEL;
-	api.malloc = (void *(*)(size_t))ADDR_MALLOC;
-
-	api.printf("  started PlayBrew!  ");
-
-	//by moving the path into memory instead of reading off of the firmware it might not crash with the string reading error... 
-	//time to find out
-
-	char *payloadPath = "/playbrew/payload.bin";
-	char *pathInMem = api.malloc(22); //22 is strlen(payloadPath) + 1
-	api.strcpy(pathInMem, payloadPath);
-	void *fh = api.ucfs_open(pathInMem, (unsigned int)0x83);
-
-
-	if (fh == NULL) {
-		api.printf("   could not open /playbrew/payload.bin! Returning!   ");
-		return;
-	}
-
-	unsigned int filesz;
-	api.ucfs_read(fh, &filesz, sizeof(unsigned int));
-	api.ucfs_read(fh, &entryPoint, sizeof(unsigned int));
-	if (filesz % 4 != 0) {
-		payloadStart = PLAYDATE_RAM_END - (filesz + 4 - (filesz % 4));
-	}
-	else {
-		payloadStart = PLAYDATE_RAM_END - filesz;
-	}
-	
-	api.ucfs_read(fh, (void *)payloadStart, filesz);
-	api.ucfs_close(fh);
-	api.printf("  Finished all reading and closed the payload!  ");
-
-	api.printf("  running the payload!  ");
-
-	void (*entry)(PlayBrewAPI *) = (void (*)(PlayBrewAPI *))((unsigned int)(payloadStart + entryPoint) | 1);
-	entry(&api);
-
-	api.printf("  payload execution finished!  ");
-	
-	
+void playbrew_elevator(void) {
 	
 }
 
