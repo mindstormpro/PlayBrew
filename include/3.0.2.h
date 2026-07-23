0e53c9fd9b6dc8b089748c87d655892899376b06
mindstormpro <169100823+mindstormpro@users.noreply.github.com>
2026-07-22 21:09:23 -0700

    New Idea Done!
    my new idea:
    instead of loading a payload, I just run all system apps as ring 0 (aka full permissions) and normal system apps won't be affected. then you just install a special system app (the PlayBrew menu) and bang you have ACE for all of your needs

--- include/3.0.2.h
+++ include/3.0.2.h
@@ -3,4 +3,18 @@
 
 #include <stddef.h>
 
+typedef struct {
+	void (*printf)(const char *format, ...);
+	unsigned int (*unlockIfSystem)(void);
+	unsigned char (*isSysApp)(void);
+	
+} PlayBrewAPI;
+
+
+#define ADDR_PRINTF ((void (*)(const char *, ...))0x240baf51)
+#define ADDR_UNLOCKIFSYSTEM ((void (*)(void))0x24051741)
+#define ADDR_ISSYSAPP ((unsigned char (*)(void))0x24041435)
+
+
+
 #endif
--- src/loader/main.c
+++ src/loader/main.c
@@ -1,15 +1,27 @@
 #include "3.0.2.h"
 
+// not needed rn:
+//#ifdef TARGET_DVT1
+//#define PLAYDATE_RAM_END ((void *)0x61000000)
+//#endif
+//#ifdef TARGET_H7D1
+//#define PLAYDATE_RAM_END ((void *)0x91000000)
+//#endif
 
-#ifdef TARGET_DVT1
-#define PLAYDATE_RAM_END ((void *)0x61000000)
-#endif
-#ifdef TARGET_H7D1
-#define PLAYDATE_RAM_END ((void *)0x91000000)
-#endif
+unsigned char isAppSystem;
 
 void playbrew_elevator(void) {
+	PlayBrewAPI api;
+
+	api.printf = (void (*)(const char *, ...))ADDR_PRINTF;
+	api.unlockIfSystem = (unsigned int (*)(void))ADDR_UNLOCKIFSYSTEM;
+	api.isSysApp = (unsigned char (*)(void))ADDR_ISSYSAPP;
+
+	api.unlockIfSystem();
+	isAppSystem = api.isSysApp();
+	if (isAppSystem != 0) {__asm__ volatile ("svc #2");} // up goes the elevator heheh
 	
+
 }
 
 #include "start.inc"
